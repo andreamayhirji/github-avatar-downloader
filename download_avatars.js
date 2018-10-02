7c5b93c1cb05afdd4fa1504e34a00353a9b61e47
Andrea <andreamayhirji@gmail.com>
2018-10-02 05:08:45 +0000

    created a function that will fetch all of the avatars from the body and add them to a folder on my computer.

--- download_avatars.js
+++ download_avatars.js
@@ -8,41 +8,35 @@ function getRepoContributors(repoOwner, repoName, handleFetchAvatarUrl) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-        'User-Agent': 'request',
-        'Authorization': secrets.GITHUB_TOKEN
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
     }
   };
 
-//this is an Asynchronous function. This is why it includes a callback.
-// when you complete this network request (could take 10 sec), call the callback and run the code inside it.
-    request(options, function(err, res, body) {
-    //res.body -- this is strandard for finding the body.
-    //JSON.parse is going to take the res.body and convert it to an array.
+
+  request(options, function (err, res, body) {
+
     var contributors = JSON.parse(res.body);
-    // console.log('this is the response ', res.body)
     contributors.forEach((contributor) => {
-        handleFetchAvatarUrl(contributor.avatar_url)
-        // console.log(contributor);
+      handleFetchAvatarUrl(contributors)
+
     });
   });
 
 }
 
 function downloadImageByURL(url, filePath) {
-
-  request.get(url)               
-       .on('error', function (err) {                                  
-         throw err; 
-       })
-       .pipe(fs.createWriteStream(filePath)); 
-
-
+  var fetchFilePath = request.get(url).pipe(fs.createWriteStream(filePath));
+  return fetchFilePath;
 }
 
-var fetchAvatarUrl = function(avatarUrl) {
-  console.log(avatarUrl);
+var fetchAvatarUrl = function (contributors) {
+  contributors.forEach(function (element) {
+    var avatarLogin = 'avatars/' + element.login + '.jpg'
+    downloadImageByURL(element.avatar_url, avatarLogin);
+  });
 }
 
 
-// getRepoContributors("jquery", "jquery", fetchAvatarUrl);
-downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg')
+getRepoContributors("jquery", "jquery", fetchAvatarUrl);
+downloadImageByURL('https://avatars3.githubusercontent.com/u/156867?v=4', 'avatars/Monica.jpg')
